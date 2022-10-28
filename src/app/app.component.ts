b4853b212a23e0eecab5a74704a2ba79ba000968
Arjun <arjun.p@stackontech.com>
2022-10-28 16:45:01 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'changelog-widget';
+
+  showDescription: boolean = false;
+  state: 'BREAKING' | 'OTHER' | 'IN PROGRESS' | 'ADDED' | 'FIXED' | 'CHANGED' | 'DEPRECATED' | 'REMOVED' = 'ADDED';
+  listOfEntries = [{state:'ADDED'}];
+
+  toggleStates(i:number){
+    this.listOfEntries.forEach((entry,index)=>{
+      if(i===index){
+        switch (entry.state){
+          case 'ADDED':
+            entry.state = 'FIXED';
+            break;
+          case 'FIXED':
+            entry.state = 'CHANGED';
+            break;
+          case 'CHANGED':
+            entry.state = 'DEPRECATED';
+            break;
+          case 'DEPRECATED':
+            entry.state = 'REMOVED';
+            break;
+          case 'REMOVED':
+            entry.state = 'BREAKING';
+            break;
+          case 'BREAKING':
+            entry.state = 'OTHER';
+            break;
+          case 'OTHER':
+            entry.state = 'IN PROGRESS';
+            break;
+          case 'IN PROGRESS':
+            entry.state = 'ADDED';
+            break;
+        }
+      }
+    })
+  };
+
+  onClickDescription(){
+    this.showDescription = !this.showDescription;
+  };
+
+  onAddEntry(){
+    this.listOfEntries.push({state:'ADDED'});
+  }
 }
